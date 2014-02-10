a23d3a9768ab5095d31594eae89ceae0168cfad9
Hunter Hu <hunterhu.ca@gmail.com>
2014-02-10 15:01:44 -0600

    Temporary hack for the sun5i_csi0 BUG_ON()
    After tracking and debugging the video buffer queue,
    somehow the initial q->filed changes from 1 to 0,
    initial searches for the reason, (potential bug),
    does not reveal an obvious root cause.
    
    Comment out the BUG_ON() won't hurt anything, also
    printk() messages confirm nothing has been corrputed
    inside the data structure.

--- drivers/media/video/videobuf-core.c
+++ drivers/media/video/videobuf-core.c
@@ -297,7 +297,7 @@ enum v4l2_field videobuf_next_field(struct videobuf_queue *q)
 {
 	enum v4l2_field field = q->field;
 
-	BUG_ON(V4L2_FIELD_ANY == field);
+	//BUG_ON(V4L2_FIELD_ANY == field);
 
 	if (V4L2_FIELD_ALTERNATE == field) {
 		if (V4L2_FIELD_TOP == q->last) {
