b6afc28b1e614f03b80da0eb205c696789ba9301
Hunter Hu <hunterhu.ca@gmail.com>
2014-07-31 19:06:05 -0500

    Kernel_cscope:select only sunxi files

--- gen_linux_kernel_cscope_files.sh
+++ gen_linux_kernel_cscope_files.sh
@@ -7,16 +7,11 @@ LNX=$HOME/projects/1030tpc-dev/sunxi-bsp/linux-sunxi
 cd / 	
 find  $LNX \
 	    -path "$LNX/arch/*" ! -path "$LNX/arch/arm*" -prune -o \
-        -path "$LNX/arch/arm/*" ! -path "$LNX/arch/arm/mach-msm*" -prune -o \
-		-path "$LNX/include/asm-*" ! -path "$LNX/include/asm-arm*" -prune -o \
-	    -path "$LNX/tmp*" -prune -o \
+        -path "$LNX/arch/arm/*" ! -path "$LNX/arch/arm/mach-sun*"  ! -path "$LNX/arch/arm/plat-sun*" -prune -o \
+		-path "$LNX/drivers*" -prune -o \
+		-path "$LNX/scripts*" -prune -o \
 		-name "*.[chxsS]" -print >$LNX/cscope.files
 
 #note: 
-#      According to "man find" notes that "the pattern match test applies to 
-#      the whole file name,so need the * at the end".
-#      That means the following line won't work:
-#          -path "$LNX/arch" ! -path "$LNX/arch/arm" -prune -o \
-#      Have to do this with a *:
-#          -path "$LNX/arch/*" ! -path "$LNX/arch/arm/*" -prune -o \
+# 1> add drivers' code as needed
 
